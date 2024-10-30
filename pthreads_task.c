c6f639728b844e09f365488e1918bea792aefc4c
imnishantdevops <157911131+imnishantdevops@users.noreply.github.com>
2024-10-30 14:34:50 +0530

    Delete pthreads_task.c

--- pthreads_task.c
+++
@@ -1,9 +0,0 @@
-#include "pthreads_task.h"
-#include <pthread.h>
-
-void process_data_pthreads(float *data, int size) {
-    // Create and manage threads for processing
-    for (int i = 0; i < size; i++) {
-        data[i] += 1;  // Example operation
-    }
-}
