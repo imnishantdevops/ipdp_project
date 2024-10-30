5f03d1b7b54db9261edad0c39fd0723230020639
imnishantdevops <157911131+imnishantdevops@users.noreply.github.com>
2024-10-30 14:55:57 +0530

    Create noise_elimination.h

---
+++ src/noise_elimination.h
@@ -0,0 +1,7 @@
+// noise_elimination.h
+#ifndef NOISE_ELIMINATION_H
+#define NOISE_ELIMINATION_H
+
+void eliminate_noise(float *data, int num_records, int num_parameters);
+
+#endif // NOISE_ELIMINATION_H
